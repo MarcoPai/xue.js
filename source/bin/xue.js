f65b70903065eb0843bd200ae7ba358e3146b696
Marco.Pai <mr.pai@msn.com>
2015-10-19 18:40:26 +0800

    测试

--- source/bin/xue.js
+++ source/bin/xue.js
@@ -24,8 +24,16 @@
 // run(process.argv.slice(2)); 
 
 var program = require('commander');
-program
-  .version('0.0.1')
+
+//定义参数,以及参数内容的描述  
+program  
+    .version('0.0.1')
+    .usage('[options] [value ...]')
+    .option('-m, --message <string>', 'a string argument')
+    .option('-i, --integer <n>', 'input a integet argument.', parseInt) 
+    .option('-f, --float <f>', 'input a float arg', parseFloat)
+    .option('-l, --list <items>', 'a list', list)
+    .option('-r, --range <a>..<b>', 'a range', range)
   .option('-C, --chdir <path>', 'change the working directory')
   .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
   .option('-T, --no-tests', 'ignore test hook')
@@ -77,4 +85,48 @@ program
     console.log('deploying "%s"', env);
   });
 
-program.parse(process.argv);
+// program.parse(process.argv);
+
+
+
+function range (val) {  
+    return val.split('..').map(Number);  
+}  
+  
+function list (val) {  
+    return val.split(',')  
+}  
+  
+  
+//添加额外的文档描述  
+program.on('help', function() {  
+    console.log('   Examples:')  
+    console.log('')  
+    console.log('       # input string, integer and float')  
+    console.log('       $ ./nodecmd.js -m \"a string\" -i 1 -f 1.01')  
+    console.log('')  
+     
+    console.log('       # input range 1 - 3')  
+    console.log('       $ ./nodecmd.js -r 1..3')  
+    console.log('')  
+     
+    console.log('       # input list: [1,2,3]')  
+    console.log('       $ ./nodecmd.js -l 1,2,3')  
+    console.log('')  
+});  
+  
+//解析commandline arguments  
+program.parse(process.argv)  
+  
+//输出结果  
+console.info('--messsage:')  
+console.log(program.message);  
+  
+console.info('--integer:')  
+console.log(program.integer)  
+  
+console.info('--range:')  
+console.log(program.range)  
+  
+console.info('--list:')  
+console.log(program.list)  
