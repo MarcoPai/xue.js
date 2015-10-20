6805099e8426eb73d563e22e9cead601c59b0d48
Marco.Pai <mr.pai@msn.com>
2015-10-20 18:34:39 +0800

    。

---
+++ source/lib/init.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var init = module.exports = {};
+
+init.log = function (txt) {
+    console.log(txt);
+};
+
--- source/lib/module.js
+++ source/lib/module.js
@@ -1,2 +1,7 @@
 'use strict';
 
+var Module = module.exports = {};
+
+Module.log = function (args) {
+    console.log(args);
+};
